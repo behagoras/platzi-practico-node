8c38f21827fda1887a2546ef129280b6edecfa53
David Behar <david@behar.io>
2020-02-23 22:29:56 -0600

    Make env variables work

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
+require('dotenv').config()
+
 const config = {
   api: {
     port: process.env.API_PORT || 3000
   },
   jwt: {
     secret: process.env.SECRET || 'laVidaEsBella*!'
+  },
+  mysql: {
+    host: process.env.MYSQL_HOST || 'localhost',
+    database: process.env.MYSQL_DATABASE || 'store',
+    user: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_PASSWORD || ''
   }
 }
 
