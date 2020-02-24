e64d210ef8803f13da4c84b001397796f59b2e98
David Behar <david@behar.io>
2020-02-23 22:28:03 -0600

    Make security middleware work by calling next() function

--- api/components/user/secure.js
+++ api/components/user/secure.js
@@ -1,18 +1,19 @@
 const auth = require('../../../auth')
-const response = require('../../../network/response')
 
 const checkAuth = (action) => {
-  const middleware = (req, res, next) => {
+  function middleware (req, res, next) {
     const owner = req.body.id
     switch (action) {
       case 'update':
-        auth.check.own(req, owner) && response.success(req, res, `${req.body.name} editado`, 201)
+        auth.check.own(req, owner)
+        next()
         break
+
       default:
         next()
     }
   }
+
   return middleware
 }
-
 module.exports = checkAuth
